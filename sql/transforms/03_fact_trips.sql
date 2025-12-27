64f613074e2f7816190da4b7093738fe896f47e3
Zeva Patu Assyadid <122035948+zevapt@users.noreply.github.com>
2025-12-27 18:17:25 +0700

    Update 03_fact_trips.sql

--- sql/transforms/03_fact_trips.sql
+++ sql/transforms/03_fact_trips.sql
@@ -1,1 +1,10 @@
-
+CREATE TABLE fact_trips AS
+SELECT
+    card_id,
+    MIN(tap_time) AS boarding_time,
+    MAX(tap_time) AS alighting_time,
+    MIN(stop_id) AS from_stop_id,
+    MAX(stop_id) AS to_stop_id,
+    direction
+FROM fact_taps
+GROUP BY card_id, DATE(tap_time), direction;
