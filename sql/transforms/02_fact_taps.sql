26a5ff9193ae0916bb5abf70c1d3894f2fa5ee5e
Zeva Patu Assyadid <122035948+zevapt@users.noreply.github.com>
2025-12-27 18:17:00 +0700

    Update 02_fact_taps.sql

--- sql/transforms/02_fact_taps.sql
+++ sql/transforms/02_fact_taps.sql
@@ -1,1 +1,22 @@
-
+INSERT INTO fact_taps (
+    tap_id,
+    card_id,
+    tap_type,
+    tap_time,
+    stop_id,
+    direction,
+    vehicle_id
+)
+SELECT
+    r.tap_id,
+    r.card_id,
+    r.tap_type,
+    r.tap_time,
+    s.stop_id,
+    s.direction,
+    v.vehicle_id
+FROM staging_taps_raw r
+JOIN stops s
+  ON r.stop_name = s.stop_name
+JOIN vehicles v
+  ON r.vehicle_code = v.vehicle_code;
