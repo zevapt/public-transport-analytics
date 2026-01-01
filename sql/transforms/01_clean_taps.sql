4f95a9d4adc359c4849a223864371fe365ca5915
Zeva Patu Assyadid <122035948+zevapt@users.noreply.github.com>
2026-01-01 22:16:10 +0700

    Update 01_clean_taps.sql

--- sql/transforms/01_clean_taps.sql
+++ sql/transforms/01_clean_taps.sql
@@ -1,3 +1,15 @@
-SELECT *
-FROM staging_taps_raw
-WHERE tap_type IN ('IN', 'OUT');
+CREATE TABLE IF NOT EXISTS clean_taps AS
+SELECT
+    tap_id,
+    TRIM(card_id) AS card_id,
+    CASE
+        WHEN UPPER(tap_type) IN ('IN', 'TAP IN') THEN 'IN'
+        WHEN UPPER(tap_type) IN ('OUT', 'TAP OUT') THEN 'OUT'
+    END AS tap_type,
+    tap_time,
+    TRIM(stop_name) AS stop_name,
+    TRIM(vehicle_code) AS vehicle_code
+FROM stg_taps
+WHERE
+    tap_time IS NOT NULL
+    AND card_id IS NOT NULL;
